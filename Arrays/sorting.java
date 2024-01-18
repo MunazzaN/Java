fa43ff34fed2910b78fdf59dd95d9cf5407d7521
MunazzaN <156056181+MunazzaN@users.noreply.github.com>
2024-01-18 14:18:32 -0600

    commit

---
+++ Arrays/sorting.java
@@ -0,0 +1,24 @@
+public class sorting {
+    public static void main(String[] args) {
+        // Hardcoded array
+        int[] myArray = {64, 34, 25, 12, 22, 11, 90};
+
+        // Bubble Sort
+        int n = myArray.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (myArray[j] > myArray[j + 1]) {
+                    int temp = myArray[j];
+                    myArray[j] = myArray[j + 1];
+                    myArray[j + 1] = temp;
+                }
+            }
+        }
+
+        // Print sorted array
+        System.out.print("Sorted array: ");
+        for (int num : myArray) {
+            System.out.print(num + " ");
+        }
+    }
+}
